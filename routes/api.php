5f0edc0fcc80ade9b04942578f07642c21972a7c
Camilo Sampedro <camilo.sampedro@udea.edu.co>
2017-03-19 21:06:08 -0500

    :sparkles: API routes for cities added

--- routes/api.php
+++ routes/api.php
@@ -17,15 +17,28 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/api/personas/{id?}', ['middleware' => 'auth.basic', function($id = null) {
+Route::get('/api/v1/people/{id?}', ['middleware' => 'auth.basic', function($id = null) {
   if($id == null) {
-    $people = App\Person::all(array('id', 'email', 'city'));
+    $people = App\Person::all(array('id', 'name', 'email', 'city_id'));
   } else {
-    $people = App\Person::find($id, array('id', 'email', 'city'));
+    $people = App\Person::find($id, array('id', 'name', 'email', 'city_id'));
   }
   return Response::json(array(
     'error' => false,
     'people' => $people,
+    'status_code' => 200
+  ));
+}]);
+
+Route::get('/api/v1/cities/{id?}', ['middleware' => 'auth.basic', function($id = null) {
+  if($id == null) {
+    $cities = App\City::all(array('id', 'name'));
+  } else {
+    $cities = App\City::find($id, array('id', 'name'));
+  }
+  return Response::json(array(
+    'error' => false,
+    'city' => $cities,
     'status_code' => 200
   ));
 }]);
