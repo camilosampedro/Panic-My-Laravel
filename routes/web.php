0b687b9464eea5178ccc624c84134f35002df87c
Camilo Sampedro <camilo.sampedro@udea.edu.co>
2017-03-22 22:40:12 -0500

    :whale: laravel db script added

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,32 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/prueba/{n?}', function($n = null) {
+  return $n;
+});
+
+Route::get('/api/v1/people/{id?}', function ($id = null) {
+    if ($id == null) {
+        $people = App\Person::all(array('id', 'name', 'email', 'city_id'));
+    } else {
+        $people = App\Person::find($id, array('id', 'name', 'email', 'city_id'));
+    }
+    return Response::json(array(
+    'error' => false,
+    'people' => $people,
+    'status_code' => 200
+  ));
+});
+
+Route::get('/api/v1/cities/{id?}', ['middleware' => 'auth.basic', function ($id = null) {
+    if ($id == null) {
+        $cities = App\City::all(array('id', 'name'));
+    } else {
+        $cities = App\City::find($id, array('id', 'name'));
+    }
+    return Response::json(array(
+    'error' => false,
+    'city' => $cities,
+    'status_code' => 200
+  ));
+}]);
